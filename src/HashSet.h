057416d99a35b81a1e3cf32edf446cc23421b2ad
Ramunas Genys <genys.ramunas@gmail.com>
2018-02-13 00:31:37 +0000

    added HashSet (to be templated)

---
+++ src/HashSet.h
@@ -0,0 +1,42 @@
+
+#ifndef HASHMAP_H
+#define HASHMAP_H
+
+#include <string>
+#include <vector>
+#include <algorithm>
+#include <random>
+#include <initializer_list>
+#include "LinkedList.h"
+
+
+class HashSet {
+public:
+	HashSet(const std::initializer_list<std::string>& list);
+	HashSet();
+	~HashSet();
+	int size();
+	bool empty();
+	void insert(const std::string& value);
+	void erase(const std::string& value);
+	bool contains(const std::string& value);
+	void traverse();
+	unsigned int buckets();
+private:
+	
+	long long int preHash(const std::string& value);
+	long long int hash(long long int prehashKey, size_t size);
+
+	void grow();
+	void shrink();
+
+private:
+
+	std::vector< SList<std::string> > set;
+	unsigned int elements;
+	static const unsigned int startingSize;
+	int a, b;
+};
+
+
+#endif
