0304e325d2d0a3175b5da6a3c45196a17f8c3dfb
Ramunas Genys <genys.ramunas@gmail.com>
2018-02-18 17:03:56 +0000

    added an option to provide custom hashing functions for the user

--- src/HashSet.h
+++ src/HashSet.h
@@ -10,8 +10,19 @@
 #include "SList.h"
 
 
-
 template <typename val_type>
+struct PreHash {
+	unsigned long long int operator()(const val_type& value) const {
+		int key = 7;
+		std::string word = std::to_string(value);
+		for (const auto& letter : word) {
+			key = key * 31 + (int)letter;
+		}
+		return std::abs(key);
+	}
+};
+
+template <typename val_type, typename prehash = PreHash<val_type> >
 class HashSet {
 public:
 	HashSet(const std::initializer_list<val_type>& list);
@@ -37,16 +48,17 @@ private:
 	std::vector< SList<val_type> > set;
 	unsigned int elements;
 	static const unsigned int startingSize;
+	prehash prehashF;
 	int a, b;
 };
 
 
 
-template<typename val_type>
-const unsigned int HashSet<val_type>::startingSize = 20;
+template<typename val_type, typename prehash = PreHash<val_type> >
+const unsigned int HashSet<val_type, prehash>::startingSize = 20;
 
-template<typename val_type>
-HashSet<val_type>::HashSet() : elements(0), set(startingSize) {
+template<typename val_type, typename prehash = PreHash<val_type> >
+HashSet<val_type, prehash>::HashSet() : elements(0), set(startingSize) {
 
 	std::random_device rd;
 	std::mt19937 generator(rd());
@@ -54,8 +66,8 @@ HashSet<val_type>::HashSet() : elements(0), set(startingSize) {
 	a = U(generator);
 	b = U(generator);
 }
-template<typename val_type>
-HashSet<val_type>::HashSet(const std::initializer_list<val_type>& list)
+template<typename val_type, typename prehash = PreHash<val_type> >
+HashSet<val_type, prehash>::HashSet(const std::initializer_list<val_type>& list)
 	: HashSet<val_type>()
 
 {
@@ -63,19 +75,19 @@ HashSet<val_type>::HashSet(const std::initializer_list<val_type>& list)
 		insert(name);
 	}
 }
-template<typename val_type>
-HashSet<val_type>::~HashSet() {
+template<typename val_type, typename prehash = PreHash<val_type> >
+HashSet<val_type, prehash>::~HashSet() {
 }
 
-template<typename val_type>
-bool HashSet<val_type>::contains(const val_type& value) {
+template<typename val_type, typename prehash = PreHash<val_type> >
+bool HashSet<val_type, prehash>::contains(const val_type& value) {
 	long long int prehashKey = preHash(value);
 	long long int trueKey = hash(prehashKey, set.size());
 	return set[trueKey].find(value);
 }
 
-template<typename val_type>
-long long int HashSet<val_type>::preHash(const val_type& value) {
+template<typename val_type, typename prehash = PreHash<val_type> >
+long long int HashSet<val_type, prehash>::preHash(const val_type& value) {
 	int key = 7;
 	for (const auto& letter : value) {
 		key = key * 31 + (int)letter;
@@ -83,8 +95,8 @@ long long int HashSet<val_type>::preHash(const val_type& value) {
 	return std::abs(key);
 }
 
-template<typename val_type>
-long long int HashSet<val_type>::hash(long long int prehashKey, size_t size) {
+template<typename val_type, typename prehash = PreHash<val_type> >
+long long int HashSet<val_type, prehash>::hash(long long int prehashKey, size_t size) {
 	long long int oldKey = prehashKey;
 	int prime = 8188057;
 
@@ -94,8 +106,8 @@ long long int HashSet<val_type>::hash(long long int prehashKey, size_t size) {
 	return trueKey;
 }
 
-template<typename val_type>
-void HashSet<val_type>::insert(const val_type& value) {
+template<typename val_type, typename prehash = PreHash<val_type> >
+void HashSet<val_type, prehash>::insert(const val_type& value) {
 	long long int prehashKey = preHash(value);
 	long long int trueKey = hash(prehashKey, set.size());
 
@@ -106,8 +118,8 @@ void HashSet<val_type>::insert(const val_type& value) {
 	}
 }
 
-template<typename val_type>
-void HashSet<val_type>::erase(const val_type& value) {
+template<typename val_type, typename prehash = PreHash<val_type> >
+void HashSet<val_type, prehash>::erase(const val_type& value) {
 	if (empty()) {
 		return;
 	}
@@ -120,8 +132,8 @@ void HashSet<val_type>::erase(const val_type& value) {
 	}
 }
 
-template<typename val_type>
-void HashSet<val_type>::shrink() {
+template<typename val_type, typename prehash = PreHash<val_type> >
+void HashSet<val_type, prehash>::shrink() {
 	if (set.size() > startingSize && set.size() >= elements * 4) {
 		std::vector<SList<val_type>> temp(set.size() / 2);
 		for (const auto& bucket : set) {
@@ -137,8 +149,8 @@ void HashSet<val_type>::shrink() {
 }
 
 
-template<typename val_type>
-void HashSet<val_type>::grow() {
+template<typename val_type, typename prehash = PreHash<val_type> >
+void HashSet<val_type, prehash>::grow() {
 	if (set.size() > elements) {
 		return;
 	}
@@ -155,29 +167,25 @@ void HashSet<val_type>::grow() {
 
 }
 
-template<typename val_type>
-unsigned int HashSet<val_type>::buckets() {
+template<typename val_type, typename prehash = PreHash<val_type> >
+unsigned int HashSet<val_type, prehash>::buckets() {
 	return (unsigned int)set.size();
 }
 
-template<typename val_type>
-void HashSet<val_type>::traverse() {
+template<typename val_type, typename prehash = PreHash<val_type> >
+void HashSet<val_type, prehash>::traverse() {
 	for (const auto& bucket : set) {
 		bucket.print();
 	}
 }
 
-template<typename val_type>
-int HashSet<val_type>::size() {
+template<typename val_type, typename prehash = PreHash<val_type> >
+int HashSet<val_type, prehash>::size() {
 	return elements;
 }
 
-template<typename val_type>
-bool HashSet<val_type>::empty() {
+template<typename val_type, typename prehash = PreHash<val_type> >
+bool HashSet<val_type, prehash>::empty() {
 	return elements == 0;
 }
-
-
-
-
 #endif
