87451895470c788b8e7f14ed6b6d67b36ff99a39
Ramunas Genys <genys.ramunas@gmail.com>
2018-08-23 23:37:27 +0100

    Added AVL property to the tree

--- src/BinaryTree.h
+++ src/BinaryTree.h
@@ -7,8 +7,7 @@ namespace DataStructures {
 	/*
 	TODO:
 	1. Make it an AVL Tree
-	2. Crete Map and Set that inherit from BinaryTree
-	3. Add iterator support
+	2. Crete Map and Set that inherit from BinaryTre
 	*/
 
 	template <typename val_type>
@@ -18,17 +17,23 @@ namespace DataStructures {
 	private:
 
 		struct Node {
+
 			val_type data;
 			Node* left;
 			Node* right;
+			// - is left heavy, + is right heavy, 0 is neutral
+			short load_factor;
+
 			Node(const val_type& value) :
 				data(value),
 				right(nullptr),
-				left(nullptr) {}
+				left(nullptr),
+			    load_factor(0) {}
 			Node(val_type&& value) :
 				data(std::move(value)),
 				right(nullptr),
-				left(nullptr) {}
+				left(nullptr),
+				load_factor(0) {}
 		};
 
 	private:
@@ -42,13 +47,10 @@ namespace DataStructures {
 			using pointer = Node*;
 			using reference = Node & ;
 			using value = val_type;
-
+			// constructors
 		public:
 
 			forward_iterator() : current(nullptr) {}
-			forward_iterator(const forward_iterator* source) :
-				current(source->current), 
-				parents(source->parents) {}
 
 			forward_iterator(const forward_iterator& source) : 
 				current(source.current), 
@@ -56,11 +58,12 @@ namespace DataStructures {
 
 			forward_iterator& operator=(const forward_iterator& rhs) {
 				current = rhs.current;
+				parents= rhs.parents;
 				return *this;
 			}
 
 		private:
-			forward_iterator(pointer c, std::stack<pointer>&& p) : current(c), parents(std::move(p)) {}
+			forward_iterator(pointer c, std::stack<pointer> p) : current(c), parents(p) {}
 		public:
 
 			forward_iterator& operator++() {
@@ -164,7 +167,7 @@ namespace DataStructures {
 					current = current->right;
 				}
 				else {
-					return iterator(current, std::move(parents));
+					return iterator(current, parents);
 				}
 			}
 			return end();
@@ -198,15 +201,28 @@ namespace DataStructures {
 		}
 
 		void insert(const val_type& value) {
+
+			if (find(value) != end()) {
+				return;
+			}
+
 			if (head == nullptr) {
 				++size_;
 				head = new Node(value);
 			}
+
+			bool need_rebalancing = false;
+			std::stack<Node*> parents;
 			Node* parent = nullptr;
 			Node* current = head;
 			bool quit = false;
+
 			while (!quit) {
 				if (value < current->data) {
+
+					parents.push(current);
+					--current->load_factor;
+
 					parent = current;
 					current = current->left;
 					if (current == nullptr) {
@@ -217,6 +233,9 @@ namespace DataStructures {
 
 				}
 				else if (value > current->data) {
+
+					parents.push(current);
+					++current->load_factor;
 					parent = current;
 					current = current->right;
 					if (current == nullptr) {
@@ -228,8 +247,118 @@ namespace DataStructures {
 				else {
 					quit = true; // the key already exists
 				}
+				if (parent != nullptr) {
+
+					if (parent->load_factor > 1 || parent->load_factor < -1) {
+						need_rebalancing = true;
+					}
+
+				}
+				
+			}
+			if (need_rebalancing && !parents.empty()) {
+				rebalance(parents);
 			}
 		}
+		private:
+
+			void rebalance(std::stack<Node*>& parents) {
+				int total_compensation = 0;
+				while (!parents.empty()) {
+
+					Node* current = parents.top();
+					// oldCurrent is needed for adjusting the parent's load factor after a rotation
+					Node* old_current = current;
+					parents.pop();
+
+					
+					Node* parent;
+					parent = parents.empty() ? nullptr : parents.top();
+					// used to determine how to adjust the parents height after a rotation
+					Node* old_parent_left = (parent == nullptr) ? nullptr : parent->left;
+					// node is doubly left heavy
+					if ((current)->load_factor < -1) {
+
+						// left child is left heavy
+						if ((current)->left->load_factor < 0) {
+							++total_compensation;
+							
+							current->load_factor = 0; // += 2;
+							current->left->load_factor = 0; //++;
+
+							RightRotate(current, parent);
+						}
+						// left child is balanced
+						else if ((current)->left->load_factor == 0) {
+
+							current->load_factor++;
+							current->left->load_factor++;
+
+							RightRotate(current, parent);
+						}
+
+						// left child is right heavy
+						else {
+							
+							++total_compensation;
+							current->load_factor = 0; //+= 2;
+							current->left->load_factor = 0; //--;
+
+							LeftRotate((current)->left, current);
+							RightRotate(current, parent);
+						}
+					}
+					// node is doubly right heavy
+					else if ((current)->load_factor > 1) {
+
+						// right child is right heavy
+						if (current->right->load_factor > 0) {
+							++total_compensation;
+							
+							current->load_factor = 0; //-= 2;
+							current->right->load_factor = 0; //--;
+
+							LeftRotate(current, parent);
+						}
+						// right child is balanced
+						else if (current->right->load_factor == 0) {
+							current->load_factor--;
+							current->right->load_factor--;
+
+							LeftRotate(current, parent);
+
+						}
+						// right child is left heavy
+						else {
+
+							++total_compensation;
+							current->load_factor = 0; //-= 2;
+							current->right->load_factor = 0; // ++;
+
+							RightRotate((current)->right, current);
+							LeftRotate(current, parent);
+						}
+					}
+
+					if (parent != nullptr) {
+						// using old values for comparison because current ones change when rotating
+						// but we need the old values in order to determine its parent's load factor
+						if (old_parent_left == old_current) {
+							parent->load_factor += total_compensation;
+						}
+						else {
+							parent->load_factor -= total_compensation;
+						}
+					}
+					// experiment
+					if (total_compensation != 0) {
+						return;
+					}
+				}
+			}
+
+		public:
+
 		void erase(const val_type& value) {
 
 			Node* parent = nullptr;
@@ -246,80 +375,83 @@ namespace DataStructures {
 				}
 				// found the item to be deleted
 				else {
-					--size_;
-					// the item has no children
-					if (current->left == nullptr && current->right == nullptr) {
-						// means current is head
-						if (parent == nullptr) {
-							delete current;
-							head = nullptr;
-						}
-						else {
-							if (parent->left == current) {
-								parent->left = nullptr;
-							}
-							else {
-								parent->right = nullptr;
-							}
-
-							delete current;
-							current = nullptr;
-						}
-						
-						return;
-					}
-					// has no left children
-					else if (current->left == nullptr) {
-						if (current == head) {
-							head = current->right;
-
-						}
-						else {
-							if (parent->left == current) {
-								parent->left = current->right;
-							}
-							else {
-								parent->right = current->right;
-							}
-						}
-						delete current;
-						current = nullptr;
-						return;
-					}
-					// has no right children
-					else if (current->right == nullptr) {
-
-						if (current == head) {
-							head = current->left;
-
-						}
-						else {
-							if (parent->left == current) {
-								parent->left = current->left;
-							}
-							else {
-								parent->right = current->left;
-							}
-						}
-
-						delete current;
-						current = nullptr;
-						return;
-					}
-					// has both children
-					else {
-						auto[minParent, min] = rightMin(current);
-						// TODO: change later to prevent copying
-						current->data = min->data;
-						eraseRightMinimum(&minParent, &min);
-						return;
-					}
-				}
-
+					delete_found()
 			}
 		}
 
 	private:
+
+		void delete_found() {
+			--size_;
+			// no children
+			if (current->left == nullptr && current->right == nullptr) {
+				// means current is head
+				if (parent == nullptr) {
+					delete current;
+					head = nullptr;
+				}
+				else {
+					if (parent->left == current) {
+						parent->left = nullptr;
+					}
+					else {
+						parent->right = nullptr;
+					}
+
+					delete current;
+					current = nullptr;
+				}
+
+				return;
+			}
+			// has no left children
+			else if (current->left == nullptr) {
+				if (current == head) {
+					head = current->right;
+
+				}
+				else {
+					if (parent->left == current) {
+						parent->left = current->right;
+					}
+					else {
+						parent->right = current->right;
+					}
+				}
+				delete current;
+				current = nullptr;
+				return;
+			}
+			// has no right children
+			else if (current->right == nullptr) {
+
+				if (current == head) {
+					head = current->left;
+
+				}
+				else {
+					if (parent->left == current) {
+						parent->left = current->left;
+					}
+					else {
+						parent->right = current->left;
+					}
+				}
+
+				delete current;
+				current = nullptr;
+				return;
+			}
+			// has both children
+			else {
+				auto[minParent, min] = rightMin(current);
+				// TODO: change later to prevent copying
+				current->data = min->data;
+				eraseRightMinimum(&minParent, &min);
+				return;
+			}
+		}
+		}
 		Node* minimum(Node* current, std::stack<Node*>& stack) {
 			
 			while (current->left != nullptr) {
@@ -353,6 +485,7 @@ namespace DataStructures {
 		}
 
 		void RightRotate(Node* currNode, Node* parent) {
+
 			Node* temp = currNode->left;
 			currNode->left = temp->right;
 			temp->right = currNode;
@@ -371,13 +504,24 @@ namespace DataStructures {
 		}
 
 		public:
+			// test functions
+			int max_height_test() {
+				return maxH(head);
+			}
 			void right_rotate_test() {
-				RightRotate(head, nullptr);
+				RightRotate(head->right, head);
 			}
 			void left_rotate_test() {
-				LeftRotate(head, nullptr);
+				LeftRotate(head->right, head);
 			}
 			private:
+				int maxH(Node* current) {
+					if (current == nullptr) {
+						return -1;
+					}
+					return 1 + std::max(maxH(current->left), maxH(current->right));
+				}
+
 		// performs a left rotation (seems to work so far)
 		void LeftRotate(Node* currNode, Node* parent) {
 
